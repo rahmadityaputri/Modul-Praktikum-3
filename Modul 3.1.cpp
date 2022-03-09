94e2a17315c317378322d89c362ef2d2eb912ff9
rahmadityaputri <100739030+rahmadityaputri@users.noreply.github.com>
2022-03-09 13:08:06 +0700

    Add files via upload

---
+++ Modul 3.1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+
+int HitungKarakter(const char*s){
+	int x = 0;
+		for (;*s!='\0';s++){
+			++x;
+		}
+		return x;
+}
+
+main (void){
+	char String [100];
+	cout<<"Inputkan Kata : ";
+	cin.getline(String,100);cout<<endl;
+	cout<<"Jumlah Karakter : "<<HitungKarakter(String)<<" Huruf "<<endl;
+}
